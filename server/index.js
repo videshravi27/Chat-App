e822f400388e16017d4213540f29523a11f2d56f
Videsh Ravi <videshravi27@gmail.com>
2024-11-20 23:46:12 +0530

    mongodb connection

--- server/index.js
+++ server/index.js
@@ -3,11 +3,15 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 
+const authRoutes = require("./routes/auth.route");
+
+const connectDB = require("./lib/db");
+
 const app = express();
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
-  console.log("Connected to MongoDB");
-  app.listen(process.env.PORT, () => {
-    console.log("Server is running on port ${process.env.PORT}");
-  });
+app.use("/auth", authRoutes);
+
+app.listen(process.env.PORT, () => {
+  connectDB()
+  console.log("Server is running on port " + process.env.PORT);
 });
