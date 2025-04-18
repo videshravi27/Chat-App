74c7b49e83fb4495fd37ce544ec260bf4f63a8d8
Videsh Ravi <videshravi27@gmail.com>
2025-04-18 17:00:29 +0530

    changes in utils.js file

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -7,9 +7,9 @@ const generateToken = (userid, res) => {
 
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
-    httpOnly: true,  //prevent XSS attacks cross-site scripting attacks
-    sameSite: "strict",
-    secure: process.env.NODE_ENV !== "development",
+    httpOnly: true, //prevent XSS attacks cross-site scripting attacks
+    sameSite: "None",
+    secure: true,
     expires: new Date(Date.now() + 86400000),
   });
 
