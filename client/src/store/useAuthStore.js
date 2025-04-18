5aea5777b64f679286e8c2a060d1d9553c3952a4
Videsh Ravi <videshravi27@gmail.com>
2025-04-18 16:47:44 +0530

    resolved error in useAuthStore

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = "http://localhost:4000";
+const BASE_URL = "https://chat-app-xzm4.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
