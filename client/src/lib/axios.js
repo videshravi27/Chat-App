21ced92be14673b512a7066bed112e642d5f3665
Videsh Ravi <videshravi27@gmail.com>
2025-04-18 16:34:40 +0530

    backend url added

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:4000",
-    withCredentials: true
-})
+  baseURL: "https://chat-app-xzm4.onrender.com",
+  withCredentials: true,
+});
