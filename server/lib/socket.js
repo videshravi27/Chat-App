b35203f02e703748be43d589e551e4e4426cf1f0
Videsh Ravi <videshravi27@gmail.com>
2025-04-18 16:43:57 +0530

    localhost url removed

--- server/lib/socket.js
+++ server/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://chat-app-ten-ecru.vercel.app"],
+    origin: ["https://chat-app-ten-ecru.vercel.app"],
   },
 });
 
